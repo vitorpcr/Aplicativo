e6917fe2ea82c6a8edc3a3c664e31a27b7dbbd03
VitorCanedo <vitorpcrcanedo@gmail.com>
2022-08-15 10:09:23 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Non-Clothing</Text>
+      <Text>vitor</Text>
       <StatusBar style="auto" />
     </View>
   );
